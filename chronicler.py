ffe6eb25b5c15005a8f9bd71a1db56754c232514
Brian Alexander <balexander04@gmail.com>
2020-02-21 08:20:33 -0600

    Adding more logging

--- chronicler.py
+++ chronicler.py
@@ -34,6 +34,7 @@ def run_chronicler():
     LOGGER.info("Start of script")
 
     for user in LIST_OF_USERS_TO_FOLLOW:
+        LOGGER.info(f"starting collection for user: @{user}")
         user_quoted_retweets = find_quoted_tweets(user=user)
         collect_and_post_tweets(user_quoted_retweets)
 
