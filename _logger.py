835ca4236acd5064be2825222ad504fad221dc30
Brian Alexander <balexander04@gmail.com>
2020-04-06 18:21:34 -0500

    Starting to improve logging.

--- _logger.py
+++ _logger.py
@@ -1,10 +1,23 @@
 import logging
-from sys import stdout
+import os
 
+MODULE_DIR_PATH = os.path.dirname(__file__)
+LOG_DIR_PATH = os.path.join(MODULE_DIR_PATH, "logs")
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s: %(message)s",
-    stream=stdout,
-)
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler(os.path.join(LOG_DIR_PATH, "chronicler.log"))
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+LOGGER.addHandler(fh)
+LOGGER.addHandler(ch)
