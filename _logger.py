83de5e93553908319d924c8ab5de0709c86360de
Brian Alexander <balexander04@gmail.com>
2020-04-06 23:12:07 -0500

    Improve variable names for logger.

--- _logger.py
+++ _logger.py
@@ -4,20 +4,22 @@ import os
 MODULE_DIR_PATH = os.path.dirname(__file__)
 LOG_DIR_PATH = os.path.join(MODULE_DIR_PATH, "logs")
 
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
+LOGGER = logging.getLogger("twitter_chronicler")
+LOGGER.setLevel(logging.INFO)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 
 # create file handler which logs even debug messages
-fh = logging.FileHandler(os.path.join(LOG_DIR_PATH, "chronicler.log"))
-fh.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(os.path.join(LOG_DIR_PATH, "chronicler.log"))
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
 # create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-# create formatter and add it to the handlers
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.ERROR)
+console_handler.setFormatter(formatter)
 
 # add the handlers to the logger
-LOGGER.addHandler(fh)
-LOGGER.addHandler(ch)
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(file_handler)
