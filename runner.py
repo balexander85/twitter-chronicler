efc51db0e7aa03c4dcfff6ca6f90f3437aa9ed57
Brian Alexander <balexander04@gmail.com>
2020-08-14 22:41:57 -0500

    Adding time performance for some statistics

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from filelock import FileLock, Timeout
 
@@ -22,4 +23,7 @@ def main():
 
 
 if __name__ == "__main__":
+    start_time = time.perf_counter()
     main()
+    elapsed = time.perf_counter() - start_time
+    LOGGER.info(f"{__file__} executed in {elapsed:0.2f} seconds.")
